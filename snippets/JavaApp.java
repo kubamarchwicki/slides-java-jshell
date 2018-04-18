d07318ee3e2f644e779864bf3752611cd625bcb3
kubamarchwicki <kuba@marchwicki.pl>
2018-04-18 11:23:52 +0200

    Updating JShell snippets to Java10

--- snippets/JavaApp.java
+++ snippets/JavaApp.java
@@ -36,7 +36,9 @@ public class JavaApp {
 
   public void run() {
       Service http = Service.ignite();
-      http.port(8888);
+      http.port(Optional
+        .ofNullable(System.getProperty("PORT"))
+        .map(Integer::valueOf).orElse(8888));
       http.staticFiles.externalLocation("/home/kubam/workspaces/slides/java9-jshell/ui");
       http.init();
 
